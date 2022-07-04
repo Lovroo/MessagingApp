f0a1e08c7515cbaa9df36a2996a4f99e8145fc78
lovro <lovro.napotnik@scv.si>
2022-07-04 17:20:10 +0200

    styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,17 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+$( document ).ready(function() {
+    $('.ui.dropdown')
+        .dropdown();
+
+});
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '@doabit/semantic-ui-sass'
-$( document ).ready(function() {
-    $('.ui.dropdown')
-        .dropdown();
-
-});
 
 Rails.start()
 Turbolinks.start()
